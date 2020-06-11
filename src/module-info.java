bf053812ea8e4a370933f4068ecbb680c34fbded
Meme1sh_Fetish <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-06-11 10:22:46 -0700

    yes
    yes

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module mod3L0 {
+	exports _01_else_if._1_robot_color_chooser;
+	exports _01_else_if._3_high_low;
+	exports _01_else_if._2_crazy_cat_lady;
+	exports _02_loop_variables._3_robot_walk_of_fame;
+	exports _02_loop_variables._2_shiny_objects;
+	exports _03_modulo._4_fizz_buzz;
+	exports _99_extra;
+
+	requires java.desktop;
+	requires robot;
+}
